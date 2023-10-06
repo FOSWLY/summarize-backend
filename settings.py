454c918a49769ce9f3795bb4ecadd066c9a66ae3
unknown <toil.official@yandex.com>
2023-10-06 21:39:25 +0300

    added default settings

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ from config.load import load_env
 
 load_env()
 
-PORT = os.environ.get('PORT') or 3312
-API_KEY = os.environ.get('API_KEY') or ''
-YANDEX_COOKIE = os.environ.get('YANDEX_COOKIE') or ''
+PORT = os.environ.get('PORT', 3312)
+API_KEY = os.environ.get('API_KEY', '')
+YANDEX_COOKIE = os.environ.get('YANDEX_COOKIE', '')
