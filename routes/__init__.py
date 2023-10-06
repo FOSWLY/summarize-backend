da0adb672577466a0754f60783869da3003b487a
unknown <toil.official@yandex.com>
2023-10-06 21:39:05 +0300

    added health router

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from routes.endpoints import summarize
+from routes.endpoints import health, summarize
 
 api_router = APIRouter()
 api_router.include_router(summarize.router, tags = ['Summarize'])
+api_router.include_router(health.router, tags = ['Health'])
