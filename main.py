a0ca7a07216f7b68155cf572297a8ee67c34a8eb
unknown <toil.official@yandex.com>
2023-10-30 21:54:04 +0300

    some improvements

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import asyncio
 from hypercorn.asyncio import serve
 
-from server import start, app
+from server import start
 
 if __name__ == '__main__':
-    config = asyncio.run(start())
-    asyncio.run(serve(app, config))
+    config, app = asyncio.run(start())
+    asyncio.run(serve(app, config)) # type: ignore
--- server.py
+++ server.py
@@ -1,12 +1,12 @@
-from core.app import app
 from hypercorn.config import Config
+
+from core.app import app
 from core.logger import init_logging
 from routes import api_router
 
 
-app.include_router(api_router)
-
 async def start():
-    config = Config().from_pyfile('config/hypercorn_config.py')
+    app.include_router(api_router)
+    config = Config().from_pyfile('hypercorn_config.py')
     init_logging()
-    return config
+    return config, app
