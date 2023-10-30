feba32caa175869408d745a474e3bc8a5f359d53
unknown <toil.official@yandex.com>
2023-10-30 21:50:08 +0300

    added api version info

--- schemas/health.py
+++ schemas/health.py
@@ -2,7 +2,5 @@ from pydantic import BaseModel, Field
 
 
 class HealthResponse(BaseModel):
-    status: str = Field(
-        description='Returns ok if FOSWLY API is available',
-        default='ok'
-    )
+    status: str = Field(description='Returns ok if FOSWLY API is available')
+    version: str = Field(description='Returns version of FOSWLY API')
